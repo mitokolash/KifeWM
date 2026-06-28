d7c2bea92a1f3915ce3613b9eb96330736778ac1
mitokolash <nycojn@gmail.com>
2026-06-28 13:51:11 -0300

    Changing the name from b to zig because it's better

--- build.zig
+++ build.zig
@@ -3,28 +3,24 @@ const Build = std.Build;
 
 const Scanner = @import("wayland").Scanner;
 
-pub fn build(b: *Build) !void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+pub fn build(zig: *Build) !void {
+    const scanner = Scanner.create(zig, .{});
+    const wayland = zig.createModule(.{ .root_source_file = scanner.result });
 
-    const scanner = Scanner.create(b, .{});
-
-    const wayland = b.createModule(.{ .root_source_file = scanner.result });
-
-    scanner.addCustomProtocol(b.path("protocol/river-xkb-bindings-v1.xml"));
-    scanner.addCustomProtocol(b.path("protocol/river-window-management-v1.xml"));
+    scanner.addCustomProtocol(zig.path("protocol/river-xkb-bindings-v1.xml"));
+    scanner.addCustomProtocol(zig.path("protocol/river-window-management-v1.xml"));
 
     scanner.generate("river_xkb_bindings_v1", 2);
     scanner.generate("wl_seat", 7);
     scanner.generate("wl_compositor", 6);
     scanner.generate("river_window_manager_v1", 1);
 
-    const exe = b.addExecutable(.{
+    const exe = zig.addExecutable(.{
         .name = "kifewm",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
+        .root_module = zig.createModule(.{
+            .root_source_file = zig.path("src/main.zig"),
+            .target = zig.standardTargetOptions(.{}),
+            .optimize = zig.standardOptimizeOption(.{}),
             .link_libc = true,
         }),
     });
@@ -32,15 +28,12 @@ pub fn build(b: *Build) !void {
     exe.root_module.addImport("wayland", wayland);
     exe.root_module.linkSystemLibrary("wayland-client", .{});
 
-    b.installArtifact(exe);
+    zig.installArtifact(exe);
 
-    const run_cmd = b.addSystemCommand(&.{ "river", "-c" });
-
-    // run_cmd.setEnvironmentVariable("WAYLAND_DISPLAY", "wayland-1");
+    const run_cmd = zig.addSystemCommand(&.{ "river", "-c" });
     run_cmd.addArtifactArg(exe);
+    run_cmd.step.dependOn(zig.getInstallStep());
 
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    const run_step = b.step("run", "Run River with this window manager as the controller");
+    const run_step = zig.step("run", "Run River with this window manager as the controller");
     run_step.dependOn(&run_cmd.step);
 }
